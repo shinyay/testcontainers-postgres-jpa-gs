6fd31891956ed44999d01fd707a50a9b3d471203
shinyay <shinya.com@gmail.com>
2022-05-11 19:13:40 +0900

    Add save book function

--- src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
+++ src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
@@ -1,10 +1,13 @@
 package io.spring.shinyay.test.service
 
+import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.repository.BookRepository
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(val bookRepository: BookRepository) {
+
+    fun saveBook(book: Book) = bookRepository.save(book)
 
     fun findAll() = bookRepository.findAll()
 }
