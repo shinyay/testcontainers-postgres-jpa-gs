d675ca8349380ad7804f9ed950194053c9f25939
shinyay <shinya.com@gmail.com>
2022-05-11 19:27:46 +0900

    Add POST Controller

--- src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
@@ -3,6 +3,7 @@ package io.spring.shinyay.test.controller
 import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.service.BookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,4 +13,8 @@ class BookController(val bookService: BookService) {
 
     @GetMapping("/books")
     fun getBooks(): MutableList<Book> = bookService.getBooks()
+
+    @PostMapping("/books")
+    fun storeBook(book: Book): Book = bookService.storeBook(book)
 }
+
