7aba3e0e521e599700df4224c1ef4cd52ec1d7ee
shinyay <shinya.com@gmail.com>
2022-05-11 17:49:00 +0900

    Add Repository for Book

---
+++ src/main/kotlin/io/spring/shinyay/test/BookRepository.kt
@@ -0,0 +1,8 @@
+package io.spring.shinyay.test
+
+import io.spring.shinyay.test.entity.Book
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository : JpaRepository<Book, Long>
