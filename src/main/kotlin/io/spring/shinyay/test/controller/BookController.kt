f15af01ea3e7a6a16ad039ca783db777a88d4c9d
shinyay <shinya.com@gmail.com>
2022-05-11 19:24:44 +0900

    Add root API for Book service

--- src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
@@ -1,4 +1,9 @@
 package io.spring.shinyay.test.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
 class BookController {
 }
