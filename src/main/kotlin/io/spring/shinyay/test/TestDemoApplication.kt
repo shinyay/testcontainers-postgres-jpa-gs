2925dc393da5c13e49f2852c239fe4950d3a5210
shinyay <shinya.com@gmail.com>
2022-05-11 15:43:45 +0900

    Initial Commit

---
+++ src/main/kotlin/io/spring/shinyay/test/TestDemoApplication.kt
@@ -0,0 +1,11 @@
+package io.spring.shinyay.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestDemoApplication>(*args)
+}
---
+++ src/test/kotlin/io/spring/shinyay/test/TestDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.spring.shinyay.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
