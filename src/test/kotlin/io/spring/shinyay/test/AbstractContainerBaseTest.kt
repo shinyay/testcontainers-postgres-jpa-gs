d7e768f6593e8a3e3dffe235f6163627971c4325
shinyay <shinya.com@gmail.com>
2022-05-12 19:16:24 +0900

    Add dynamic property registration for DB Username

--- src/test/kotlin/io/spring/shinyay/test/AbstractContainerBaseTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/AbstractContainerBaseTest.kt
@@ -1,14 +1,24 @@
 package io.spring.shinyay.test
 
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.utility.DockerImageName
 
-abstract class AbstractContainerBaseTest(var POSTGRES_SQL_CONTAINER: PostgreSQLContainer<*>?) {
+abstract class AbstractContainerBaseTest() {
 
     companion object {
+        @Container
         val postgres = PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:14.2-alpine"))
         init {
             postgres.start()
+        }
+
+        @DynamicPropertySource
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+
         }
     }
 }
