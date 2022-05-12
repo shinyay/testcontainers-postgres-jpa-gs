46eb05d82b36c6a3b41e9e97b5294f35c5f64c40
shinyay <shinya.com@gmail.com>
2022-05-12 17:37:07 +0900

    Add a singleton cotainer for PostgreSQL

--- src/test/kotlin/io/spring/shinyay/test/AbstractContainerBaseTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/AbstractContainerBaseTest.kt
@@ -1,6 +1,14 @@
 package io.spring.shinyay.test
 
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 
 abstract class AbstractContainerBaseTest(var POSTGRES_SQL_CONTAINER: PostgreSQLContainer<*>?) {
+
+    companion object {
+        val postgres = PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:14.2-alpine"))
+        init {
+            postgres.start()
+        }
+    }
 }
