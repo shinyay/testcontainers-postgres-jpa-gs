afb1ff9c6854d8a3eed8c46d2357f1f32d11f87b
shinyay <shinya.com@gmail.com>
2022-05-13 19:21:01 +0900

    Add BeforeEach function for Testing

--- src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
@@ -1,6 +1,7 @@
 package io.spring.shinyay.test
 
 import io.spring.shinyay.test.repository.BookRepository
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,4 +18,6 @@ class BookPostgreSQLContainerTest(
     val repository: BookRepository
 ) : AbstractContainerBaseTest() {
 
+    @BeforeEach
+    fun setUp() = repository.deleteAll()
 }
