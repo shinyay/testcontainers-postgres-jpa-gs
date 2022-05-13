56fd7d4555d563cdb1f1326f374138a0ed29f606
shinyay <shinya.com@gmail.com>
2022-05-13 17:00:41 +0900

    Add MockMvc Annotation

--- src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
@@ -1,9 +1,11 @@
 package io.spring.shinyay.test
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest
+@AutoConfigureMockMvc
 class BookPostgreSQLContainerTest : AbstractContainerBaseTest() {
 }
