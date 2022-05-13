c41157e0d16de36c4ffa218c0dacd48c96778a7a
shinyay <shinya.com@gmail.com>
2022-05-13 23:28:05 +0900

    Add given and when for saving test

--- src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
@@ -1,5 +1,6 @@
 package io.spring.shinyay.test
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.repository.BookRepository
 import org.junit.jupiter.api.BeforeEach
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
@@ -25,6 +27,11 @@ class BookPostgreSQLContainerTest(
 
     @Test
     fun should_be_able_to_save_one_book() {
+        // given
         val book = Book(author = "Shinya Yanagihara", title = "Spring Boot in Action", year = 2020)
+
+        // when & then
+        mockMvc.perform(post("/books")
+            .contentType("application/json"))
     }
 }
