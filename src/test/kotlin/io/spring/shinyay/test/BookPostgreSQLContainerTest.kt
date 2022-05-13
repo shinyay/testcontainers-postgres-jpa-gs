1f135891ed9cadbb8d6bf92c1d5466c36a156605
shinyay <shinya.com@gmail.com>
2022-05-13 17:00:16 +0900

    Add SpringBootTest annotation

--- src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
@@ -1,7 +1,9 @@
 package io.spring.shinyay.test
 
+import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
+@SpringBootTest
 class BookPostgreSQLContainerTest : AbstractContainerBaseTest() {
 }
