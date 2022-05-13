f17ef6b067f060dc495225130a63d6106097420a
shinyay <shinya.com@gmail.com>
2022-05-13 17:06:03 +0900

    Add constructor injection for MockMVC

--- src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/BookPostgreSQLContainerTest.kt
@@ -1,11 +1,17 @@
 package io.spring.shinyay.test
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
-class BookPostgreSQLContainerTest : AbstractContainerBaseTest() {
+class BookPostgreSQLContainerTest(
+    @Autowired
+    val mockMvc: MockMvc
+) : AbstractContainerBaseTest() {
+
 }
