8e050e900b15b659cb1eb42095fc459a3babba56
shinyay <shinya.com@gmail.com>
2022-05-16 08:26:06 +0900

    Add Response Status for gettion books

--- src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
@@ -2,10 +2,12 @@ package io.spring.shinyay.test.controller
 
 import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.service.BookService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 class BookController(val bookService: BookService) {
 
     @GetMapping("/books")
+    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
     fun getBooks(): MutableList<Book> = bookService.getBooks()
 
     @PostMapping("/books")
