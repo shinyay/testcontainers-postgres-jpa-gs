59d49a94312a0e1118f71281609955aaf272a96b
shinyay <shinya.com@gmail.com>
2022-05-16 12:55:43 +0900

    Add book repository

--- src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
@@ -1,5 +1,7 @@
 package io.spring.shinyay.test.unit
 
+import io.spring.shinyay.test.repository.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -8,4 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RepositoryTest {
+
+    @Autowired
+    lateinit var repository: BookRepository
 }
