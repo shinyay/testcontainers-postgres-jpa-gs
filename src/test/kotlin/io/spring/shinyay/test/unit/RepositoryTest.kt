7397ff748bffe8acd170f88a59a757cf58ef28c3
shinyay <shinya.com@gmail.com>
2022-05-16 12:54:59 +0900

    Add data test class anntation

--- src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
@@ -1,7 +1,11 @@
 package io.spring.shinyay.test.unit
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RepositoryTest {
 }
