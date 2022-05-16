d7371c5be46de78c75f0647eab26a71cebbf304f
shinyay <shinya.com@gmail.com>
2022-05-16 11:28:02 +0900

    M

--- src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
+++ src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
@@ -1,6 +1,7 @@
 package io.spring.shinyay.test.service
 
 import io.spring.shinyay.test.entity.Book
+import io.spring.shinyay.test.logger
 import io.spring.shinyay.test.repository.BookRepository
 import org.springframework.stereotype.Service
 
@@ -10,4 +11,9 @@ class BookService(val bookRepository: BookRepository) {
     fun storeBook(book: Book) = bookRepository.save(book)
 
     fun getBooks(): MutableList<Book> = bookRepository.findAll()
+
+    fun getBooksByName(name: String): List<Book> {
+        logger.info("getBookByName: $name")
+        return bookRepository.findByName(name)
+    }
 }
