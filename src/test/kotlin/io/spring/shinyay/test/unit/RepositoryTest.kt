321aff99103935c8330e87606ed87b4ea4e4af29
shinyay <shinya.com@gmail.com>
2022-05-17 11:08:52 +0900

    Add a test for saving and retrieving

--- src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/unit/RepositoryTest.kt
@@ -1,6 +1,7 @@
 package io.spring.shinyay.test.unit
 
 import io.spring.shinyay.test.AbstractContainerBaseTest
+import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.repository.BookRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Order
@@ -23,5 +24,20 @@ class RepositoryTest : AbstractContainerBaseTest() {
     fun should_be_able_to_find_no_books_at_initial() {
         val result = repository.findAll()
         assertThat(result.count()).isEqualTo(0)
+    }
+
+    @Test
+    @Order(2)
+    fun should_be_able_to_save_book() {
+        val book1 = Book(title = "Spring Boot in Action", author = "Craig Walls", year = 2020)
+        val book2 = Book(title = "Spring Boot in Practice", author = "Joshua White", year = 2022)
+
+        repository.run {
+            save(book1)
+            save(book2)
+        }
+        val result = repository.findAll()
+
+        assertThat(result.count()).isEqualTo(1)
     }
 }
