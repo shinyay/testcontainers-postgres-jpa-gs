394d158e1dcb3e75e67d72fe5a33316b7a676ece
shinyay <shinya.com@gmail.com>
2022-05-17 14:51:01 +0900

    Modify entity definition with Response Entity instead of ResposeStatus

--- src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/test/controller/BookController.kt
@@ -3,6 +3,7 @@ package io.spring.shinyay.test.controller
 import io.spring.shinyay.test.entity.Book
 import io.spring.shinyay.test.service.BookService
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,18 +14,30 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
-class BookController(val bookService: BookService) {
+class BookController(val service: BookService) {
 
     @GetMapping("/books")
-    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
-    fun getBooks(): MutableList<Book> = bookService.getBooks()
+//    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
+    fun getBooks(): ResponseEntity<MutableList<Book>> {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getBooks())
+    }
 
     @GetMapping("/books/{name}")
-    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
-    fun getBooksByAuthor(@PathVariable name: String): List<Book> = bookService.getBooksByAuthor(name)
+//    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
+    fun getBooksByAuthor(@PathVariable name: String): ResponseEntity<List<Book>> {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getBooksByAuthor(name))
+    }
+
+    @GetMapping("/book/{id}")
+//    @ResponseStatus(HttpStatus.OK, reason = "Successfully retrieved books")
+    fun getBookById(@PathVariable id: Long): ResponseEntity<Book> {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getBookById(id))
+    }
 
     @PostMapping("/books")
-    @ResponseStatus(HttpStatus.CREATED, reason = "Book created")
-    fun storeBook(@RequestBody book: Book): Book = bookService.storeBook(book)
+//    @ResponseStatus(HttpStatus.CREATED, reason = "Book created")
+    fun storeBook(@RequestBody book: Book): ResponseEntity<Book> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.storeBook(book))
+    }
 }
 
