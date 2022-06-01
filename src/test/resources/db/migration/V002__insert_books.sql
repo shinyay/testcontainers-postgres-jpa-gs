c7a004238db7a737e635c3e31a7c719f972a5a2a
shinyay <shinya.com@gmail.com>
2022-06-01 13:26:36 +0900

    Modify primary key not to asigne because of auto incremetal

--- src/test/resources/db/migration/V002__insert_books.sql
+++ src/test/resources/db/migration/V002__insert_books.sql
@@ -1,3 +1,3 @@
-INSERT INTO books VALUES (1, 'Spring Framework 6', 'shinyay', 2022 );
-INSERT INTO books VALUES (2, 'Spring Boot 3', 'shinyay', 2022 );
-INSERT INTO books VALUES (3, 'Spring Cloud', 'yanagiharas', 2021 );
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Framework 6', 'shinyay', 2022 );
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Boot 3', 'shinyay', 2022 );
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Cloud', 'yanagiharas', 2021 );
