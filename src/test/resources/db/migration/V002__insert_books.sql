13d517c731a61e018c0a14d733ee23c005c7e75e
shinyay <shinya.com@gmail.com>
2022-06-01 12:12:00 +0900

    Add a sql to add books data

---
+++ src/test/resources/db/migration/V002__insert_books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books VALUES (1, 'Spring Framework 6', 'shinyay', 2022 );
+INSERT INTO books VALUES (2, 'Spring Boot 3', 'shinyay', 2022 );
+INSERT INTO books VALUES (3, 'Spring Cloud', 'yanagiharas', 2021 );
