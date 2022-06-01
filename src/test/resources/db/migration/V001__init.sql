9d44d66e85d61d0a407c6fb2de3725d091327fd6
shinyay <shinya.com@gmail.com>
2022-06-01 12:00:47 +0900

    Modify data type

--- src/test/resources/db/migration/V001__init.sql
+++ src/test/resources/db/migration/V001__init.sql
@@ -3,6 +3,6 @@ CREATE TABLE books
     id SERIAL NOT NULL,
     title varchar(255) NOT NULL,
     author  varchar(20)  NOT NULL,
-    publication_year integer NOT NULL,
+    publication_year int NOT NULL,
     PRIMARY KEY (id)
 );
