ef7573fc05f00b7dc10f9759e4682a208f4d2cbf
shinyay <shinya.com@gmail.com>
2022-06-08 18:21:20 +0900

    Add flyway script

---
+++ src/main/resources/db/migration/V002__insert_books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Framework 6', 'shinyay', 2022 );
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Boot 3', 'shinyay', 2022 );
+INSERT INTO books(title, author, publication_year) VALUES ('Spring Cloud', 'yanagiharas', 2021 );
