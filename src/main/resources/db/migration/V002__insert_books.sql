9978ffb2ba79e421b77039f9bdaffa654c29b521
shinyay <shinya.com@gmail.com>
2022-06-09 16:55:26 +0900

    Modify test data

--- src/main/resources/db/migration/V002__insert_books.sql
+++ src/main/resources/db/migration/V002__insert_books.sql
@@ -1,4 +1,3 @@
 INSERT INTO books(title, author, publication_year) VALUES ('Spring Framework 6', 'shinyay', 2022 );
 INSERT INTO books(title, author, publication_year) VALUES ('Spring Boot 3', 'shinyay', 2022 );
 INSERT INTO books(title, author, publication_year) VALUES ('Spring Cloud', 'yanagiharas', 2021 );
-INSERT INTO books(title, author, publication_year) VALUES ('Spring in Action', 'yanagiharas', 2021 );
