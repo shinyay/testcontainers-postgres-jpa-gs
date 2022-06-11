00dc1ea8242af8b824441b6ce98e3ca51b4dc91b
shinyay <shinya.com@gmail.com>
2022-06-11 19:47:26 +0900

    Add the test for multiple specific books for title

--- src/test/kotlin/io/spring/shinyay/test/integration/BookPostgreSQLContainerTest.kt
+++ src/test/kotlin/io/spring/shinyay/test/integration/BookPostgreSQLContainerTest.kt
@@ -131,6 +131,7 @@ class BookPostgreSQLContainerTest(
             .andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$[1].id").value(2))
+            .andExpect(jsonPath("$[1].title").value("Spring Boot 3"))
     }
 
 //    @Test
