3b516ccf57be8d3eabf61469e95334dd55616a55
shinyay <shinya.com@gmail.com>
2022-06-17 17:00:49 +0900

    Add update logic

--- src/main/kotlin/io/spring/shinyay/test/repository/BookRepository.kt
+++ src/main/kotlin/io/spring/shinyay/test/repository/BookRepository.kt
@@ -8,4 +8,12 @@ import org.springframework.stereotype.Repository
 interface BookRepository : JpaRepository<Book, Long> {
 
     fun findByAuthor(author: String): MutableList<Book>
+
+    fun updateBook(id: Long, book: Book): Book {
+        val bookToUpdate = findById(id).get()
+        bookToUpdate.title = book.title
+        bookToUpdate.author = book.author
+        bookToUpdate.year = book.year
+        return save(bookToUpdate)
+    }
 }
