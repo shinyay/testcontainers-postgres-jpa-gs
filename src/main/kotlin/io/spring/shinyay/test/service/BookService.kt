168ee3095217a7ca287d468898c11dcec5bca278
shinyay <shinya.com@gmail.com>
2022-06-20 18:19:24 +0900

    Add Resonse Status for CREATED

--- src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
+++ src/main/kotlin/io/spring/shinyay/test/service/BookService.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service
 @Service
 class BookService(val repository: BookRepository) {
 
-    fun storeBook(book: Book) = repository.save(book)
+    fun storeBook(book: Book) = ResponseEntity.status(HttpStatus.CREATED).body(repository.save(book))
 
     fun getBooks(): MutableList<Book> = repository.findAll()
 
